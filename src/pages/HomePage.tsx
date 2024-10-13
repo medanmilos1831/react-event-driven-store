3a0bc84f3538f7c1b2a956226004b53f6cfe1307
medanmilos1831 <medanmilos1831@gmail.com>
2024-10-13 15:21:44 +0200

    fix

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,59 +1,25 @@
-import { PERSON_ACTION_TYPES } from '../store/person.types';
-import { useDispatch, useSelector, useStoreClient } from '../context';
-import { DOG_ACTION_TYPES } from '../store/dog.types';
+import { useDispatch, useSelector } from '../context';
 export const HomePage = () => {
-  const ctx = useStoreClient();
   const dispatch = useDispatch();
   const { value } = useSelector(
     (state: any) => {
-      console.log('state', state);
       return {
-        personAge: state.person.age,
-        dogAge: state.dog.age,
+        personAge: state.age,
       };
     },
-    [
-      PERSON_ACTION_TYPES.INC_AGE,
-      PERSON_ACTION_TYPES.UPDATE_NAME,
-      DOG_ACTION_TYPES.INC_AGE_DOG,
-    ]
+    ['INC_AGE']
   );
-  console.log('value', value);
   const incAge = () => {
     dispatch({
-      type: PERSON_ACTION_TYPES.INC_AGE,
-      payload: 40,
+      type: 'INC_AGE',
+      payload: 1,
     });
-  };
-
-  const updateName = () => {
-    dispatch({
-      type: PERSON_ACTION_TYPES.UPDATE_NAME,
-      payload: 'Marko',
-    });
-  };
-
-  const logState = () => {
-    console.log('logState', ctx);
   };
 
   return (
     <div>
       <h1>HomePage {value.personAge}</h1>
-      <h1>HomePage {value.dogAge}</h1>
       <button onClick={incAge}>inc age</button>
-      <button
-        onClick={() => {
-          dispatch({
-            type: DOG_ACTION_TYPES.INC_AGE_DOG,
-            payload: 40,
-          });
-        }}
-      >
-        inc age dog
-      </button>
-      <button onClick={updateName}>update name</button>
-      <button onClick={logState}>logState</button>
     </div>
   );
 };
